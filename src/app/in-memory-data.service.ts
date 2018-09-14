2f27d7e96589418d44e1b03d3e2853f672469aa6
Acar <marcello.acar@accenture.com>
2018-09-14 16:28:19 -0300

    final code - add seach, add and delete a hero

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,17 +3,16 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Mr. 1' },
-      { id: 12, name: 'Narco 2' },
-      { id: 13, name: 'Bombasto 3' },
-      { id: 14, name: 'Celeritas 4' },
-      { id: 15, name: 'Magneta 5' },
-      { id: 16, name: 'RubberMan 6' },
-      { id: 17, name: 'Dynama 7' },
-      { id: 18, name: 'Dr IQ 8' },
-      { id: 19, name: 'Magma 9' },
-      { id: 20, name: 'Tornado' },
-      { id: 21, name: 'Mr Marcel(l)o' }
+      { id: 11, name: 'Mr. Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
     ];
     return {heroes};
   }
